df04478d24e21c65a1283d812ff750610abc56d1
Maksym Sditanov <msditanov@200volts.com>
2016-12-30 23:47:05 +0200

    Add skeleton for configuration test

---
+++ tests/TestConfigurationLoad.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "Configuration.h"
+
+struct ConfigurationLoad : public ::testing::Test
+{
+        void SetUp()
+        {
+        }
+
+        void TearDown()
+        {
+        }
+};
+
+TEST_F(ConfigurationLoad, init)
+{
+	Configuration configuration;	
+}
+
+TEST_F(ConfigurationLoad, noFile)
+{
+	Configuration conf;
+
+	bool result;
+	result = conf.load("/fdafasfsdfasfasfsafsdf");
+	GTEST_ASSERT(!result);
+}
